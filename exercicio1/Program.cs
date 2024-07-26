bfbbd34b558234ed64b042043487854fc0cb6afb
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-07-26 18:18:09 -0300

    bhaskara

--- exercicio1/Program.cs
+++ exercicio1/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System.Globalization;
 
-double n1, n2, notaFinal;
+double n1, n2, notaFinal;// declarei as variaveis
 
-n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);// solicitei a primeira nota
+n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);// solicitei a segunda nota
 
-notaFinal = n1 + n2; 
+notaFinal = n1 + n2; // somei ambas
 
-Console.WriteLine("NOTA FINAL = " + notaFinal.ToString("F1", CultureInfo.InvariantCulture));
+Console.WriteLine("NOTA FINAL = " + notaFinal.ToString("F1", CultureInfo.InvariantCulture));// mensagem de saida
 
-if (notaFinal < 60.0){
+if (notaFinal < 60.0){// condição simples que caso seja verdadeira sera executada
     Console.WriteLine("REPROVADO");
 }
 
---
+++ exercicio2/Program.cs
@@ -0,0 +1,12 @@
+﻿//bhaskara
+
+using System.Globalization;
+
+double A, B, C, R1, R2;
+
+A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); 
+B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+
