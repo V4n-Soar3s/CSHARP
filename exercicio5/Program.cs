58007d8c6352ae111acc7eb08d6e6c347d2a18ca
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-07-26 20:02:26 -0300

    switch case

---
+++ exercicio5/Program.cs
@@ -0,0 +1,37 @@
+﻿//exercicio com switch case
+
+int x;
+string dia;
+
+x = int.Parse(Console.ReadLine());
+
+if (x == 1){
+    dia = "domingo";
+}
+else if (x == 2){
+    dia = "segunda";
+}
+else if (x == 3){
+    dia = "terça";
+}
+else if (x == 4){
+    dia = "quarta";
+}
+else if (x == 5){
+    dia = "quinta";
+}
+else if (x == 6){
+    dia = "sexta";
+}
+else if (x == 7){
+    dia = "sábado";
+}
+else{
+    dia = "valor inválido";
+}
+
+Console.WriteLine("Dia da semana: " + dia);
+Console.ReadLine();
+
+
+
