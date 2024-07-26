a3fb66e89ed7e92784198de839c6af3c7226b91d
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-07-26 17:48:35 -0300

    atualizacao

--- exercicio1/Program.cs
+++ exercicio1/Program.cs
@@ -1,3 +1,19 @@
-﻿Console.WriteLine("Olá Mundo");
+﻿using System.Globalization;
+
+double n1, n2, notaFinal;
+
+n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+notaFinal = n1 + n2; 
+
+if (notaFinal >= 60.00){
+    Console.WriteLine("NOTA FINAL = " + notaFinal.ToString("F1", CultureInfo.InvariantCulture));
+    Console.WriteLine("APROVADO");
+}
+else {
+    Console.WriteLine("NOTA FINAL = " + notaFinal.ToString("F1", CultureInfo.InvariantCulture));
+    Console.WriteLine("REPROVADO");
+}
 
 Console.ReadLine();
