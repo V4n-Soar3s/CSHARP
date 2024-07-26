8296a74462a8566fbdf75665e9c9fd9d1e084c0e
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-07-26 19:51:18 -0300

    atribuicao cumulativa

--- exercicio3/Program.cs
+++ exercicio3/Program.cs
@@ -1,22 +1,23 @@
 ﻿//como descobrir qual variavel é a menor 
 
-int x, y, z; 
+int x, y, z;
 
 String[] vet = Console.ReadLine().Split(' ');
 x = int.Parse(vet[0]);
 y = int.Parse(vet[1]);
 z = int.Parse(vet[2]);
 
-if(x < y && x < z){//est
+if (x < y && x < z)// criei um encadeamento de estruturas condicionais
+{
     Console.WriteLine("MENOR = " + x);
-} 
-else{
-    if (y < z){
-        Console.WriteLine("MENOR = " + y);
-    }
-    else{
-        Console.WriteLine("MENOR = " + z);
-    }
+}
+else if (y < z)
+{
+    Console.WriteLine("MENOR = " + y);
+}
+else
+{
+    Console.WriteLine("MENOR = " + z);
 }
 
 Console.ReadLine();
---
+++ exercicio4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Consumo de franquia (Operadores de atribuição cumulativa)
+
+using System.Globalization;
+ 
+ int minutos;
+ double conta;
+
+ conta = 50.0;
+
+ minutos = int.Parse(Console.ReadLine());
+
+
+ if (minutos > 100){
+    conta += (minutos - 100) * 2.0;//atribuicao cumulativa
+ }
+
+Console.WriteLine("Valor a pagar: R$ " + conta.ToString("F2", CultureInfo.InvariantCulture));
+
+Console.ReadLine();
+
