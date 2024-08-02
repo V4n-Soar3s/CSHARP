05e7ab0cf3aa5a3c5165cac6b9609fec9ad48b73
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-08-02 12:53:42 -0300

    estrutura for

--- exercicio1/obj/Debug/net8.0/exercicio1.AssemblyInfo.cs
+++ exercicio1/obj/Debug/net8.0/exercicio1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercicio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3e2ee56c496e2bdd8c0ec29312c81b338c326f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d552375098d964c24e5f4ed26050bbf53927f778")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercicio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercicio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- exercicio8/obj/Debug/net8.0/exercicio8.AssemblyInfo.cs
+++ exercicio8/obj/Debug/net8.0/exercicio8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercicio8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3e2ee56c496e2bdd8c0ec29312c81b338c326f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d552375098d964c24e5f4ed26050bbf53927f778")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercicio8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercicio8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
