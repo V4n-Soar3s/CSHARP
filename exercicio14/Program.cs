2e6c42ecf5f1637f487d60ffb13ef7fdaff71eee
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-08-03 20:10:51 -0300

    vetores

---
+++ exercicio14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Runtime.InteropServices;
+
+int N;//declarei a variavel n que ira ler a quantidade de vezes que o programa ira aceitar os valores
+double[] A;//declarei a variavel de vetor
+
+N = int.Parse(Console.ReadLine());// solicitei que o usuario digita a quantia 
+A = new double[N];// aqui é a instanciação 
+
+string[] s = Console.ReadLine().Split(' ');// criei o split para conseguir digitar os valores na mesma linha
+for (int i = 0; i<N; i++){
+    A[i] = double.Parse(s[i], CultureInfo.InvariantCulture);// tranformei em double a string
+}
+
+for (int i = 0; i<N; i++){
+    Console.Write(A[i].ToString("F1", CultureInfo.InvariantCulture) + " ");// solicitei pra exibir os valores
+}
+
+Console.WriteLine();
+
+double soma = 0.0;//declarei a variavel de soma recebendo o valor de 0
+for (int i=0; i<N; i++){
+    soma += A[i];// solicitei que ela some o primeiro indice
+}
+
+double media = soma / N;// declarei a variavel de media 
+
+Console.WriteLine(soma.ToString("F2", CultureInfo.InvariantCulture)); // solcitei que seja exibida a soma
+Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));// solicitei que seja exibida a media 
+
+Console.ReadLine();
