88c652da7c908c7ad668e877142e5ccb85260736
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-08-02 23:17:37 -0300

    estrutura for

---
+++ exercicio11.2/Program.cs
@@ -0,0 +1,25 @@
+﻿int N, cont_in, cont_out;
+
+N = int.Parse(Console.ReadLine());
+
+cont_in = 0;
+cont_out = 0;
+
+for (int i = 0; i < N; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    if (x >= 10 && x <= 20)
+    {
+        cont_in++;
+    }
+    else
+    {
+        cont_out++;
+    }
+}
+
+
+Console.WriteLine(cont_in + " in");
+Console.WriteLine(cont_out + " out");
+
+Console.ReadLine();
