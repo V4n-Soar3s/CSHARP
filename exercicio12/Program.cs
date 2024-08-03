d8f556781f497ad1ccfe8aecd4637557e6298684
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-08-03 17:06:07 -0300

    vetores

--- exercicio12/Program.cs
+++ exercicio12/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Vetores
+
+using System.Globalization;
+
+int N;//declarei a variavel do nº inteiro que precisa ser lido 
+double[] vet;//declarei a variavel de vetor 
+
+N = int.Parse(Console.ReadLine());// li o numero
+vet = new double[N];// declarei que o vetor ira receber o tamanho do nº que for digitado em N - instanciação do vetor, nao tem como fazer a instanciação antes de ler o valor de N 
+
+for(int i = 0; i < N ; i++){//declarei a estrutura for pra indexar os valores que vao ter as posições do vetor criado
+    vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+}
+
+for(int i=0; i<N; i++){// esse aqui vai rodar o vetor criado e apresentar os valores dele na tela 
+    Console.WriteLine(vet[i].ToString("F1", CultureInfo.InvariantCulture));
+}
+
+Console.ReadLine();
--- exercicio12/obj/Debug/net8.0/exercicio12.AssemblyInfo.cs
+++ exercicio12/obj/Debug/net8.0/exercicio12.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercicio12")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc1bc4145ec67f7e8f13456cbdf4f09efa35366c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02189440e24f99d6fc74d6b90905469aa7fa1c2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercicio12")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercicio12")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
